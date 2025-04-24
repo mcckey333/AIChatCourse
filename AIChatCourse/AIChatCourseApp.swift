50e54449aa275f2013e52c8c0631d23e4f3a0741
shreeyani98@gmail.com <shreeyani98@gmail.com>
2025-04-24 13:26:21 +1000

    Initial Commit

---
+++ AIChatCourse/AIChatCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseApp.swift
+//  AIChatCourse
+//
+//  Created by Intuin  on 24/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
