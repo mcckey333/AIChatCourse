59a0190970f44902f82137af8c742978f9bdbdb3
shreeyani98@gmail.com <shreeyani98@gmail.com>
2025-05-07 22:01:09 +1000

    [Feature] Add WelcomeView UI

--- AIChatCourse/Core/Welcome/WelcomeView.swift
+++ AIChatCourse/Core/Welcome/WelcomeView.swift
@@ -8,19 +8,65 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    
+    @State private var imageName: String = Constants.randomImage
+    
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Welcome")
-                    .frame(maxHeight: .infinity)
-                
-                NavigationLink {
-                    OnboardingCompletedView()
-                } label: {
-                    Text("Get Started")
-                        .callToActionButton()
-                }
+                ImageLoaderView(urlString: imageName)
+                    .frame(height: 500)
+                    .ignoresSafeArea()
+                title
+                ctaButtons
+                .padding(16)
+                policyLinks
+                Spacer()
             }
+        }
+    }
+    
+    private var title: some View {
+        VStack {
+            Text("AI Chat")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            Text("YouTube @ SwiftuiThinking")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+        }
+    }
+    private var ctaButtons: some View {
+        VStack {
+            NavigationLink {
+                OnboardingCompletedView()
+            } label: {
+                Text("Get Started")
+                    .callToActionButton()
+            }
+            
+            Text("Already have an account? Sign in")
+                .underline()
+                .font(.body)
+                .padding(8)
+                .onTapGesture {
+                    
+                }
+        }
+    }
+    private var policyLinks: some View {
+        HStack {
+            Link(destination: URL(string: Constants.termsOfServiceUrl)!) {
+                Text("Terms of Service")
+            }
+            Circle()
+                .fill(.accent)
+                .frame(width: 4, height: 4)
+            Link(destination: URL(string: Constants.privacyPolicyUrl)!) {
+                Text("Privacy Policy")
+            }
+            
         }
     }
 }
