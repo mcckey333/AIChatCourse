b9532ab3f092a4e4a89f6141c3ad6beff00755cf
shreeyani98@gmail.com <shreeyani98@gmail.com>
2025-05-17 16:35:52 +1000

    [Feature] CategoryCell

---
+++ AIChatCourse/Components/Views/CategoryCellView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCellView.swift
+//  AIChatCourse
+//
+//  Created by Intuin  on 17/5/2025.
+//
+
+import SwiftUI
+
+struct CategoryCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryCellView()
+}
