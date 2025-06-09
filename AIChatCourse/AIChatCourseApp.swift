c47d1f907eda6d6e96bc8449408f1d9fe1f4bc3a
shreeyani98@gmail.com <shreeyani98@gmail.com>
2025-06-09 18:58:56 +1000

    [Feature] Add Firebase

--- AIChatCourse/AIChatCourseApp.swift
+++ AIChatCourse/AIChatCourseApp.swift
@@ -4,11 +4,14 @@
 //
 //  Created by Intuin  on 24/4/2025.
 //
-
+import Firebase
 import SwiftUI
 
 @main
 struct AIChatCourseApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     @State private var appState = AppState()
     var body: some Scene {
         WindowGroup {
@@ -16,4 +19,12 @@ struct AIChatCourseApp: App {
                 .environment(appState)
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
 }
